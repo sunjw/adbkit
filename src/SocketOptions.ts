2fccd69371429c6c48c71812103b78ded834c298
YishaiYosifov <74960133+YishaiYosifov@users.noreply.github.com>
2024-11-19 20:45:44 +0200

    Allow authorization of known public keys (#540)
    Signed-off-by: Yishai Yosifov <74960133+YishaiYosifov@users.noreply.github.com>

--- src/SocketOptions.ts
+++ src/SocketOptions.ts
@@ -3,4 +3,5 @@ import Bluebird from 'bluebird';
 
 export default interface SocketOptions {
   auth?: (key: ExtendedPublicKey) => Bluebird<void | boolean>;
+  knownPublicKeys?: ExtendedPublicKey[];
 }
