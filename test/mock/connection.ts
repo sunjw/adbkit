98fd6a5b22ac0d651d549b75188fc6324deb75dc
Sun Junwen <sunjw8888@gmail.com>
2026-09-18 17:00:11 +0800

    Fix test.

--- test/mock/connection.ts
+++ test/mock/connection.ts
@@ -20,10 +20,9 @@ export default class MockConnection extends Connection {
         return this;
     }
 
-    // public write(data: string | Uint8Array, callback?: (err?: Error) => void): this {
-    public write(chunk: string | Uint8Array, cb?: (error: Error | null | undefined) => void): this {
-        this._socket.write(chunk, cb);
-        return this;
+    // public write(data: string | Uint8Array, callback?: (err?: Error) => void): boolean {
+    public write(chunk: string | Uint8Array, cb?: (error: Error | null | undefined) => void): boolean {
+        return this._socket.write(chunk, cb);
     }
 
     on(): this {
